cd7d2b566f8e570725788f770628baca07420537
wahyuakhmadfadillah <wahyufadil1140@gmail.com>
2023-11-11 12:58:36 +0700

    nilai

--- src/jaavak.java
+++ src/jaavak.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class jaavak {
+public class jaavak {
+    public static void main(String[] args) {
+        //kita akan membuat variabell
+        int x= 10;
+        System.out.println("nilai x ="+ x);
+        x = 20;
+        System.out.println("nilai x baru ="+ x);
+    }
 }
